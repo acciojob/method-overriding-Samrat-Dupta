3417007ebe5b699fa0212ed7b94f21717e731789
Samrat <samratsam0408@gmail.com>
2024-11-27 01:18:06 +0530

    overridign Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,8 +15,5 @@ public class Main {
     public static void main(String[] args) {
         B obj = new B();
         System.out.println(obj.meth());
-
-        B obj1 = new B();
-        System.out.println(obj1.meth());
     }
 }
